7671a975c4da98b27533922607619d7ca57d3157
bmit-gom <mentor.govori@espas.ch>
2020-06-29 10:48:32 +0200

    hallo tu

---
+++ Raumschiffe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Raumschiffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            RaumschiffVorlage raumschiffEins = new RaumschiffVorlage();
+
+            raumschiffEins.schiffName = "alba";
+
+            raumschiffEins.zeigeStatus();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Raumschiffe/RaumschiffVorlage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raumschiffe
+{
+   
+        class RaumschiffVorlage
+        {
+
+
+
+            private string schiffName;
+            public int geschwindigkeit = 5;
+
+
+        public void setzeName(string name)
+        {
+
+
+            schiffName = name;
+
+
+        }    
+
+
+
+        public void zeigeStatus()
+        {
+
+              Console.WriteLine("Name: {0}, Geschwindigkeit: {1}", schiffName, geschwindigkeit);
+
+
+
+        }
+
+
+
+
+
+
+
+        }
+}
